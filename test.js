74bd9f71b5952d25bbb299a013d9a3c218220bb6
Kevin Isom <kevin.isom@gmail.com>
2015-03-03 09:36:39 +1300

    Tests

---
+++ test.js
@@ -0,0 +1,45 @@
+
+
+var proxyquire =  require('proxyquire');
+var test = require("tap").test;
+    var returnedResponse = require('fs').readFileSync('./tests/results.xml', {encoding: 'utf8'});
+    var pxpost = proxyquire("./index.js", { 'request' : function (opts, callback) {
+        callback(null, null, returnedResponse);
+    }});
+    console.log(returnedResponse);
+test("PxPost tests", function(te) {
+
+    test("Make sure PxPost Returns a valid response", function (t) {
+        t.plan(1);
+
+        pxpost.submit({
+            user: '---',
+            password: '---',
+            amount: '100.00',
+            currency: 'NZD', //defaults to NZD
+            transactionType: 'Purchase', //default and currently only supported option
+            reference: 'Merchant Reference',
+            card: {
+                name: 'John Doe',
+                number: '4111111111111111 ',
+                expiry:'1015',
+                cvc2: '123'
+            }
+        }, function (err, result) {
+            if (err) {
+                //do something
+                console.log(err);
+            } else {
+                console.log(result);
+                console.log(result.Authorized);//Will be 1 for successful transaction
+                t.ok(result.Authorized === 1);
+            }
+        });
+
+ 
+    });
+
+ 
+
+    te.end();
+})
